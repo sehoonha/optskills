a290c1ef3a001e118f868dfa479e619e6a55a5ec
sehoonha <sehoon.ha@gmail.com>
2014-10-29 17:17:47 -0500

    Add a few mathematical routines

--- optskills/model/cov.py
+++ optskills/model/cov.py
@@ -4,7 +4,7 @@ import numpy as np
 class Cov(object):
     def __init__(self, dim, center, pts=None):
         self.dim = dim
-        self.center = center
+        self.m = center
 
         if pts is not None:
             self.fit(pts)
@@ -12,7 +12,25 @@ class Cov(object):
             self.C = np.identity(dim)
 
     def fit(self, pts):
+        n = len(pts)
+        dim = self.dim
+        C = np.zeros(dim, dim)
+        for x in pts:
+            d = x - self.m  # Difference from the center
+            C += d.reshape(dim, 1) * d
+        # should we specially care when n = 1?
+        C /= (n - 1)
+        self.C = C
+
+    def interploate(self, rhs):
+        # lhs = self
         pass
 
+    def generate_sample(self):
+        mean = self.m
+        cov = self.C
+        params = np.random.multivariate_normal(mean, cov)
+        return params
+
     def __str__(self):
-        return "{Cov %s diag:%s}" % (self.center, self.C.diagonal())
+        return "{Cov %s diag:%s}" % (self.m, self.C.diagonal())
--- optskills/model/mean.py
+++ optskills/model/mean.py
@@ -15,8 +15,27 @@ class Linear(object):
     def fit(self, pts):
         pass
 
+    def func(self, w, *params):
+        pass
+
     def point(self, w):
         return self.a + w * self.b
 
     def __str__(self):
         return "{Linear: %s %s}" % (self.a, self.b)
+
+
+# from scipy.optimize import curve_fit
+
+
+# def f(x, *params):
+#     print params
+#     (a, b, c) = params
+#     return a * np.exp(-b * x) + c
+
+# xdata = np.linspace(0, 4, 50)
+# y = f(xdata, 2.5, 1.3, 0.5)
+# ydata = y + 0.2 * np.random.normal(size=len(xdata))
+# popt, pcov = curve_fit(f, xdata, ydata, np.random.rand(3))
+# print popt
+# print pcov
